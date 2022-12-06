a66aa8149ffdd040cbd52bc77c2a8ff957ed60da
gcmglt <gcmglt@gmail.com>
2022-12-06 11:19:04 +0100

    Integrazione counter con manipolazione DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
-//* Dichiarazione variabili
-const counterDisplay = document.querySelector(".counter-display");
-const subtractButton = document.querySelector(".subtract-button");
-const addButton = document.querySelector(".add-button");
-const resetButton = document.querySelector(".reset-button");
+//* Manipolazione DOM e creazione componenti del counter
 const buttonsWrapper = document.querySelector(".buttons-wrapper");
+const counterContainer = document.querySelector(".counter-container");
 
-//* Funzione pulsanti -, + e RESET COUNTER
+function counterCreationHandler(tag, style, content) {
+  component = document.createElement(tag);
+  component.className = style;
+  component.innerHTML = content;
+  return component;
+}
+
+const subtractButton = counterCreationHandler("button", "btn subtract-button", "-");
+counterContainer.append(subtractButton);
+
+const counterDisplay = counterCreationHandler("span", "counter-display", "0");
+counterContainer.append(counterDisplay);
+
+const addButton = counterCreationHandler("button", "btn add-button", "+");
+counterContainer.append(addButton);
+
+const resetButton = counterCreationHandler("button", "btn reset-button", "RESET COUNTER");
+buttonsWrapper.append(resetButton);
+
+//* Funzione pulsanti
+let count = 0;
+
 buttonsWrapper.addEventListener("click", (e) => {
   if (e.target === subtractButton) {
-    --counterDisplay.innerHTML;
+    count--;
   } else if (e.target === addButton) {
-    ++counterDisplay.innerHTML;
-  } else if (e.target === resetButton) {
-    counterDisplay.innerHTML = 0;
-  } else return counterDisplay;
+    count++;
+  } else {
+    count = 0;
+  }
+
+  counterDisplay.innerHTML = count;
 });
 
 //* Styler pulsanti mouse over
